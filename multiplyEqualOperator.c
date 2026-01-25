a2a651261fe1b3e15b61e1f6d99c773d6eb4e74d
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-25 20:55:07 +0530

    Add files via upload
    Solved Multiply Equal Operator.c

---
+++ multiplyEqualOperator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    a *= b;  
+    printf("%d", a);
+    int c, d;
+    scanf("%d %d", &c, &d);
+    c *= d;  
+    printf("%d", c);
+    int e, f;
+    scanf("%d %d", &e, &f);
+    e *= f;  
+    printf("%d", e);
+    return 0;
+}
