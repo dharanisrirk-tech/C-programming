61f7f32e449b04d35465f1f1bb6b4eec74620c01
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-25 21:24:45 +0530

    Add files via upload
    Solved Two Number Using Lesser.c

---
+++ twoNumberUsingLesser.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a=5;
+    int b=10;
+    printf("%d\n",a<b);
+    int c=9;
+    int d=2;
+    printf("%d\n",c<d);
+    int e=4;
+    int f=4;
+    printf("%d\n",e<f);
+    return 0;
+}
