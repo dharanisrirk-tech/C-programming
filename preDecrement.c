778a7c1aaebe636324353d9f649735551f5bfd77
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-25 20:58:38 +0530

    Add files via upload
    Solved Post Decrement.c

---
+++ preDecrement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d", &a);
+    --a;         
+    printf("%d", a);
+    int b;
+    scanf("%d", &b);
+    --b;         
+    printf("%d", b);
+    int c;
+    scanf("%d", &c);
+    --c;         
+    printf("%d", c);
+
+    return 0;
+}
