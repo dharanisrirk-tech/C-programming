0aeb12a6f0cf5a63b7dea3124d9c38fb6d0bd200
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-25 21:24:04 +0530

    Add files via upload
    Solved Two Number Using Greater And Lesser Equal.c

---
+++ twoNumberUsingGreaterAndLesserEqual.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d %d", a >= b, a <= b);
+    int c, d;
+    scanf("%d %d", &c, &d);
+    printf("%d %d", c >= d, c <= )d;
+    return 0;
+    int e, f;
+    scanf("%d %d", &e, &f);
+    printf("%d %d", e >= f, e <= f);
+}
