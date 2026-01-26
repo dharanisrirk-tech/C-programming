25b75c7783336e40b12d005a73c29bb3987d941e
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-26 22:06:28 +0530

    Add files via upload
    Solved uppercase letter.c

---
+++ uppercaseLetter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a='G';
+    if(a>='A' && a<='Z'){
+    printf("Uppercase letter");
+   }
+    else{
+    printf("Not Uppercase letter");
+   }
+    return 0;
+}
