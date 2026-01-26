931a9207eebad395265fe8a49c79478e55d85a8e
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-26 22:31:15 +0530

    Add files via upload
    Solved sum of three triangle.c

---
+++ sumOfThreeTriangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a=60;
+    int b=60;
+    int c=60;
+    if(a+b+c==180)
+    printf("Valid triangle");
+    else
+    printf("Not Valid triangle");
+    return 0;
+}
