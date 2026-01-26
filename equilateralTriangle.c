3eff8e5f3b2aee737cef0d30a8ca0200f7d1c178
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-26 22:02:01 +0530

    Add files via upload
    Solved equilateral triangle.c

---
+++ equilateralTriangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a=5;
+    int b=5;
+    int c=5;
+    if(a==b && b==c && c==a)
+    printf("Equilateral triangle");
+    else
+    printf("Not Equilateral triangle");
+    return 0;
+}
