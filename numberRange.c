4eb16592b061d95eba18d519e1e6c4b74a2db441
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-26 22:11:57 +0530

    Add files via upload
    Solved number range.c

---
+++ numberRange.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int num = 15;
+    if (num >= 10 && num <= 20) {
+        printf("Number is within range");
+    } else {
+        printf("Number is outside range");
+    }
+    return 0;
+}
