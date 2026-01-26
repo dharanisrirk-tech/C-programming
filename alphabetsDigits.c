5a5c5634c8e227f565ba02397073d87cc2d5725a
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-26 22:07:22 +0530

    Add files via upload
    Solved alphabets digits & special character.c

---
+++ alphabetsDigits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    if(ch>='a'&& ch<='z'||ch>='A'&& ch<='Z'){
+    printf("Alphabet");
+    }
+    else if(ch>=0 && ch<=9 ){
+    printf("Digits");
+    }
+    else{
+    printf("Special Character");
+    }
+    return 0;
+}
