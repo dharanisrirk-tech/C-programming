db4d8c2b40c933aded514aa0e7e72ab5862b0f2d
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-01-26 22:24:44 +0530

    Add files via upload
    Solved vowels consonants.c

---
+++ vowelsConsonants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //char ch;
+    //scanf("%c",&ch);
+    char ch='a';
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='0'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    printf("vowel");
+    else
+    printf("consonant");
+    return 0;
+}
